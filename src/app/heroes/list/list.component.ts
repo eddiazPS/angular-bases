2bbf12803c1b1016e1e80052de3e410d728c67fa
Edgar J Diaz P <edgarjdiazp@Edgars-MacBook-Air.local>
2024-05-28 10:08:12 -0500

    ngIf-ng-template

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class ListComponent {
 
+
+  public heroNames:string []= ['Spiderman','Ironman','Hulk','She Hulk','Thor'];
+  public deletedHero? :string;
+
+  removeLastHero():void {
+   this.deletedHero = this.heroNames.pop();
+  }
+
+
+
+
+
+
 }
